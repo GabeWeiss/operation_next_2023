77e0b5a5b65ca83cb839a4cd766c9707b81a8577
Gabe Weiss <gweiss@google.com>
2023-07-18 16:02:59 -0700

    Bunch of process code. Nothing functional yet.

--- pi/main.py
+++ pi/main.py
@@ -1,5 +1,31 @@
+from google.cloud import pubsub_v1
 import RPi.GPIO as GPIO
+# system libraries
+import datetime
+import logging
+import sys
 from time import sleep, time
+
+# Set up logging. Debug messages will only go to stdout,
+# but the rest will go to the file 'operation.log'
+logger = logging.getLogger()
+console_handler = logging.StreamHandler(sys.stdout)
+console_handler.setLevel(logging.DEBUG)
+console_handler.setFormatter(logging.Formatter('%(asctime)s [%(levelname)s] %(message)s'))
+logger.addHandler(console_handler)
+file_handler = logging.FileHandler('operation.log')
+file_handler.setLevel(logging.INFO)
+file_handler.setFormatter(logging.Formatter('%(asctime)s [%(levelname)s] %(message)s'))
+logger.addHandler(file_handler)
+logger.setLevel(logging.DEBUG)
+
+# Set up pubsub globals
+publisher = pubsub_v1.PublisherClient()
+# TODO: Un-hardcode this topic
+topic = publisher.topic_path('gweiss-simple-path', 'operation-test')
+
+# Logging messages
+MSG_NON_MATCHING_PINS = "The active pin doesn't match the triggered pin."
 
 # Tweezers should write out high all the time in loop
 # Will complete circuits with the piece input pins
@@ -57,6 +83,19 @@ GPIO.output(PIN_SUCCESS_POS, GPIO.HIGH)
 # that's acceptable.
 GPIO.output(PIN_BUZZER_CONTROL, GPIO.LOW)
 
+def activate_buzzer():
+    GPIO.output(PIN_BUZZER_CONTROL, GPIO.HIGH)
+
+def deactivate_buzzer():
+    GPIO.output(PIN_BUZZER_CONTROL, GPIO.LOW)
+
+def send_pubsub_msg(pin):
+    global publisher
+    global topic
+    countdown = 15
+    data = f'{{ "local_time": "{datetime.datetime.now().isoformat()}", "pos": {str(pin)} }}'
+    publisher.publish(topic, data.encode("utf-8"))
+
 # In order to be sure all our pins have cleared setup, since
 # a few of them start high or floating, we want to be sure
 # we don't accidentally trigger any behaviors on startup
@@ -86,11 +125,23 @@ try:
                 active_time = -1
                 continue
 
-        if (GPIO.input()):
-            print(f"Pin 0: {count}")
-        elif (GPIO.input(4)):
-            print(f"Pin 4: {count}")
-        GPIO.output(1, GPIO.HIGH)
+        if (GPIO.input(PIN_PIECE_0)):
+            activate_buzzer()
+            # Sanity check, if this happens, we're probably
+            # in a bad state. Reset, log and continue
+            if active_pin != PIN_PIECE_0:
+                logging.CRITICAL(f"{MSG_NON_MATCHING_PINS}: Active: {active_pin}, Triggered: {PIN_PIECE_0}")
+                active_pin = -1
+                active_time = -1
+                continue
+
+            # If we're here, it means this is the first time seeing
+            # this pin trigger, so set the active pin and active time
+            # so we don't repeat trigger a message
+            active_pin = PIN_PIECE_0
+            active_time = cur_time
+            send_pubsub_msg(PIN_PIECE_0)
+
 
 except KeyboardInterrupt:
     print("Qutting")
