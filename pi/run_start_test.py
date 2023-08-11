142930974602b0529b2153fdeae7725f4a6ba1e2
Gabe Weiss <astorax@hotmail.com>
2023-08-11 14:14:56 -0700

    Adjusting weird time module issues again.

--- pi/run_start_test.py
+++ pi/run_start_test.py
@@ -1,7 +1,7 @@
 import RPi.GPIO as GPIO
 import operation
 import sys
-from time import sleep, time
+import time
 
 def exit_clean(err):
     GPIO.cleanup()
@@ -21,16 +21,16 @@ def test_piece(piece):
 try:
     print("**** Starting Operation hardware validation test ****")
 
-    sleep(1.0)
+    time.sleep(1.0)
 
     print("Testing buzzer and red nose LED. Should buzz for 0.5 seconds.")
 
-    sleep(1.0)
+    time.sleep(1.0)
 
     operation.activate_buzzer()
-    sleep(0.5)
+    time.sleep(0.5)
     operation.deactivate_buzzer()
-    sleep(1.0)
+    time.sleep(1.0)
 
     print("Confirm success button works, and green light turns on.\nYou have 10 seconds to push the button.")
     start_time = round(time.time() * 1000)
@@ -42,31 +42,31 @@ try:
             print("Did not receive signal from the success button within 10 seconds of test start.")
             exit_clean(1)
 
-    sleep(1.0)
+    time.sleep(1.0)
     test_piece(operation.PIN_ADAMS_APPLE)
-    sleep(1.0)
+    time.sleep(1.0)
     test_piece(operation.PIN_WISH_BONE)
-    sleep(1.0)
+    time.sleep(1.0)
     test_piece(operation.PIN_BROKEN_HEART)
-    sleep(1.0)
+    time.sleep(1.0)
     test_piece(operation.PIN_FUNNY_BONE)
-    sleep(1.0)
+    time.sleep(1.0)
     test_piece(operation.PIN_SPARE_RIBS)
-    sleep(1.0)
+    time.sleep(1.0)
     test_piece(operation.PIN_BUTTERFLIES_IN_THE_STOMACH)
-    sleep(1.0)
+    time.sleep(1.0)
     test_piece(operation.PIN_WRITERS_CRAMP)
-    sleep(1.0)
+    time.sleep(1.0)
     test_piece(operation.PIN_BREAD_BASKET)
-    sleep(1.0)
+    time.sleep(1.0)
     test_piece(operation.PIN_CHARLIE_HORSE)
-    sleep(1.0)
+    time.sleep(1.0)
     test_piece(operation.PIN_ANKLE_TO_KNEE_BONE)
-    sleep(1.0)
+    time.sleep(1.0)
     test_piece(operation.PIN_WATER_ON_THE_KNEE)
-    sleep(1.0)
+    time.sleep(1.0)
     test_piece(operation.PIN_WRENCHED_ANKLE)
-    sleep(1.0)
+    time.sleep(1.0)
 
     print("All tests successfully passed.")
     exit_clean(0)
